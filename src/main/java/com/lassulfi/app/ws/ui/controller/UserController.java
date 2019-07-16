446ed00c318564eaf6ce9fd23911674bb600d559
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-16 17:33:09 -0300

    Criados métodos para lidar com os verbos GET, POST, PUT e DELETE para a entidade usuario

--- src/main/java/com/lassulfi/app/ws/ui/controller/UserController.java
+++ src/main/java/com/lassulfi/app/ws/ui/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.lassulfi.app.ws.ui.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users")
 public class UserController {
 
+	@GetMapping
+	public String getUsers() {
+		return "GET /users was called";
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "POST /users was called";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "PUT /users was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "DELETE /users was called"; 
+	}
 }
