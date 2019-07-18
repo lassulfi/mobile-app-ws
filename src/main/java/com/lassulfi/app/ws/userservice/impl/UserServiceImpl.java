df56f96f928dd4febbe5bb8a607117ef2a5d98b2
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-17 21:52:20 -0300

    Constructor based dependency injection

--- src/main/java/com/lassulfi/app/ws/userservice/impl/UserServiceImpl.java
+++ src/main/java/com/lassulfi/app/ws/userservice/impl/UserServiceImpl.java
@@ -2,10 +2,11 @@ package com.lassulfi.app.ws.userservice.impl;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lassulfi.app.ws.shared.Utils;
 import com.lassulfi.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.lassulfi.app.ws.ui.model.response.UserRest;
 import com.lassulfi.app.ws.userservice.UserService;
@@ -14,6 +15,17 @@ import com.lassulfi.app.ws.userservice.UserService;
 public class UserServiceImpl implements UserService {
 	
 	Map<String, UserRest> users;
+	
+	Utils utils;
+	
+	public UserServiceImpl() {
+		
+	}
+	
+	@Autowired
+	public UserServiceImpl(Utils utils) {
+		this.utils = utils;
+	}
 
 	@Override
 	public UserRest createUser(UserDetailsRequestModel userDetails) {
@@ -22,7 +34,7 @@ public class UserServiceImpl implements UserService {
 		returnValue.setFirstName(userDetails.getFirstName());
 		returnValue.setLastName(userDetails.getLastName());
 		
-		String userId = UUID.randomUUID().toString();
+		String userId = utils.generateUserId();
 		returnValue.setUserId(userId);
 		
 		if(users == null) users = new HashMap<>();
